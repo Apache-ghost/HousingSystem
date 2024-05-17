f1f34307522f614a5dc87e8eb3ec61901566daea
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-05-17 18:17:59 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+import Testimonial from "./pages/Testimonial";
+
+
+const App = () => {
+  return (
+   <div>
+    <Navbar/>
+    <Hero />
+    <Testimonial/>
+    
+   </div>
+  );
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Login from "./Login";
+import Testimonial from "./Testimonial";
+
+export { Home, About, Projects, Contact, Login, Testimonial};
