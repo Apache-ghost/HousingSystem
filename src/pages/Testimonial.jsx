d7d0e52e974304f42e7db1f2469349ee56614a7b
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-05-18 12:04:16 +0200

    update the first card

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -4,9 +4,11 @@ function Testimonial() {
   return (
     <div className="card">
       <img src="./public/images/profile.jpg" className="card-image" alt="" />
-      <h2 className="card-title">Guegouo M. Guiddel</h2>
+      <h2 className="card-title">Guegouo  Guiddel</h2>
       <p className="card-text">I love this app, it is so user-friendly.</p>
     </div>
+
+    
   );
 }
 
