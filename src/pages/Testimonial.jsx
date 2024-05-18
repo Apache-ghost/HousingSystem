db3ddd9eac109addf36a1663947b8e06f0c3c2d3
Fon Chris Bright <pemmenyif@gmail.com>
2024-05-19 00:34:25 +0200

    created a css file contact.css imported contact to the app.jsx created testimonial.jsx and testimonial.css file in contact.jsx added a section with two divs the right side containing an image and the left side containing four blocks of contact

---
+++ src/pages/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Testimonial.css'
+const Testimonial = () => {
+    return (
+        <div>
+            <h1>Testimonials</h1>
+        </div>
+    );
+};
+
+export default Testimonial; 
