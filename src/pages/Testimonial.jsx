0a81d4519eba7b6a1fd9bfd87c1d3c2354b7c82c
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-05-18 12:30:20 +0200

    Third commit
    I included additional 3 peoples in the testimonial

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
+const testimonials = [
+  {
+    image: './public/images/profile.jpg',
+    name: 'Guegouo M. Guiddel',
+    text: 'I love this app, it is so user-friendly.'
+  },
+  {
+    image: './public/images/profile.jpg',
+    name: 'John Doe',
+    text: 'This app has made my life so much easier.'
+  },
+  {
+    image: './public/images/profile.jpg',
+    name: 'Jane Smith',
+    text: 'The best app Ive ever used!'
+  }
+];
+
 function Testimonial() {
   return (
-    <div className="card">
-      <img src="./public/images/profile.jpg" className="card-image" alt="" />
-      <h2 className="card-title">Guegouo  Guiddel</h2>
-      <p className="card-text">I love this app, it is so user-friendly.</p>
+    <div className="card-container">
+      {testimonials.map((testimonial, index) => (
+        <div className="card" key={index}>
+          <img src={`/images/${testimonial.image}`} className="card-image" alt="" />
+          <h2 className="card-title">{testimonial.name}</h2>
+          <p className="card-text">{testimonial.text}</p>
+        </div>
+      ))}
     </div>
-
-    
   );
 }
 
