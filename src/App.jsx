47f5f73c80c5409205ffb843889cd54c663ddf65
Fon Chris Bright <pemmenyif@gmail.com>
2024-05-19 00:38:24 +0200

    Updated package.json, src/App.jsx, src/pages/Contact.jsx, removed package-lock.json, and added yarn.lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
+import Contact from "./pages/Contact";
 import Testimonial from "./pages/Testimonial";
 
 
 const App = () => {
   return (
-   <div>
-    <Navbar/>
+   <div className="App">
+    <div>
+      <div className="white-gradient"/>
+
+      <Navbar/>
     <Hero />
     <Testimonial/>
     
+    </div>
+  
+    <Contact/>
    </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './contact.css'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='c-wrapper'>
+      <div className="padding innerwidth flexcenter c-container">
+   {/* left side */}
+   <div className="flexColStart c-left">
+   <span className='orangeText'>Our contact</span>
+   <span className='primaryText'>Easy to Contact Us</span>
+   <span className='secondaryText'>We are always ready to help by provindg quality and affordable houses that suit your taste</span>
+   </div>
+     {/* Right side */}
+     <div className="c-right">
+      <div className="image-container">
+    
+        <img src="./public\images\hero-image.png" alt=""/>
+      </div>
+     </div>
+     </div>
+    </section>
   )
 }
 
