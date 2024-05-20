3c6b006d033a0999ef31745f0b5117926a2b03dc
Madick Ange César <129757922+MadickAngeCesar@users.noreply.github.com>
2024-05-20 14:22:40 +0100

    list.jsx file from Card folder adder to add better stucture and cleaner code

---
+++ src/components/Card/list.jsx
@@ -0,0 +1,91 @@
+import JPic from "../../../public/images/hero-image.png";
+
+export const props = [
+  {
+    id: 1,
+    image: JPic,
+    cost: 20.0,
+    location: "Yaoundé, Messassi",
+    category: "For Rent",
+    description: "Appartment",
+  },
+  {
+    id: 2,
+    image: JPic,
+    cost: 0,
+    location: "Bonaberi, Douala",
+    category: "For Sale",
+    description: "Appartment",
+  },
+  {
+    id: 3,
+    image: JPic,
+    cost: 0,
+    location: "Yaoundé, Messassi",
+    category: "For Rent",
+    description: "Appartment",
+  },
+  {
+    id: 4,
+    image: JPic,
+    cost: 20.0,
+    location: "Bonaberi, Douala",
+    category: "For Sale",
+    description: "Appartment",
+  },
+  {
+    id: 5,
+    image: JPic,
+    cost: 0,
+    location: "Yaoundé, Messassi",
+    category: "For Rent",
+    description: "Appartment",
+  },
+  {
+    id: 6,
+    image: JPic,
+    cost: 0,
+    location: "Bonaberi, Douala",
+    category: "For Sale",
+    description: "Appartment",
+  },
+  {
+    id: 7,
+    image: JPic,
+    cost: 20.0,
+    location: "Yaoundé, Messassi",
+    category: "For Rent",
+    description: "Appartment",
+  },
+  {
+    id: 8,
+    image: JPic,
+    cost: 0,
+    location: "Bonaberi, Douala",
+    category: "For Sale",
+    description: "Appartment",
+  },
+  {
+    id: 9,
+    image: JPic,
+    cost: 0,
+    location: "Yaoundé, Messassi",
+    category: "For Rent",
+    description: "Appartment",
+  },
+  {
+    id: 10,
+    image: JPic,
+    cost: 20.0,
+    location: "Bonaberi, Douala",
+    category: "For Sale",
+    description: "Appartment",
+  },
+];
+
+export const proofs = [
+  { id: 1, value: "20+", var: "Years in the industry" },
+  { id: 2, value: "90%", var: "Customers satisfaction" },
+  { id: 3, value: "10K", var: "Properties Sold" },
+  { id: 4, value: "50+", var: "Country Served" },
+];
