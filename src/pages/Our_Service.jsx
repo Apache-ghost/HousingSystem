a228bab3015effac4d54ba2cf25be77cd4c62a2b
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-05-29 11:53:31 +0200

    updated the styles and text , the 3 steps session and services pages

--- src/pages/Our_Service.jsx
+++ src/pages/Our_Service.jsx
@@ -8,33 +8,39 @@ const Services = () => {
       <div className="box-container">
         <div className="box">
           <img src="./public/images/icon-1.png" alt="" />
-          <h3>buy house</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>Buy a House</h3>
+<p>Find and purchase your perfect home effortlessly.</p>
         </div>
         <div className="box">
           <img src="./public/images/icon-2.png" alt="" />
-          <h3>rent house</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>Rent a House</h3>
+<p>Find your perfect rental home with ease and comfort.</p>
         </div>
         <div className="box">
           <img src="./public/images/icon-3.png" alt="" />
-          <h3>sell house</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>Sell Your House</h3>
+<p>Sell your home quickly and easily with our expert help.</p>
+
+
+
+
+
+
         </div>
         <div className="box">
           <img src="./public/images/icon-4.png" alt="" />
-          <h3>flats and buildings</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>Flats and Buildings</h3>
+<p>Explore a variety of flats and buildings to suit your needs.</p>
         </div>
         <div className="box">
           <img src="./public/images/icon-5.png" alt="" />
-          <h3>shops and malls</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>Shops and Malls</h3>
+<p>Discover shopping and entertainment options to enjoy.</p>
         </div>
         <div className="box">
           <img src="./public/images/icon-6.png" alt="" />
-          <h3>24/7 service</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
+          <h3>24/7 Service</h3>
+<p>Get round-the-clock assistance for your needs.</p>
         </div>
       </div>
     </section>
--- src/pages/Simple_Steps.jsx
+++ src/pages/Simple_Steps.jsx
@@ -8,18 +8,18 @@ const StepsSection = () => {
       <div className="box-container">
         <div className="box">
           <img src="./public/images/step-1.png" alt="" />
-          <h3>search property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
+          <h3>Search Property</h3>
+<p>Find your ideal property effortlessly.</p>
         </div>
         <div className="box">
           <img src="./public/images/step-2.png" alt="" />
-          <h3>contact agents</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
+          <h3>Contact Agents</h3>
+<p>Connect with agents easily.</p>
         </div>
         <div className="box">
           <img src="./public/images/step-3.png" alt="" />
-          <h3>enjoy property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
+          <h3>Enjoy Property</h3>
+<p>Experience your dream property.</p>
         </div>
       </div>
     </section>
