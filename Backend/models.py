24be50d0f762f2c62fca934c6d5e696eabd1ddaa
Madick Ange César <129757922+MadickAngeCesar@users.noreply.github.com>
2024-06-13 11:07:20 +0100

    changing property id to id and adding to it auto increment

--- Backend/models.py
+++ Backend/models.py
@@ -1,7 +1,7 @@
 from config import db
 
 class Properties(db.Model):
-    property_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     seller_id = db.Column(db.Integer, unique=False, nullable=True)
     category = db.Column(db.String(80), unique=False, nullable=False)
     cost = db.Column(db.Float, unique=False, nullable=False)
@@ -11,7 +11,7 @@ class Properties(db.Model):
 
     def to_json(self):
         return {
-            "propertyId": self.property_id,
+            "id": self.id,
             "sellerId": self.seller_id,
             "category": self.category,
             "cost": self.cost,
@@ -21,11 +21,10 @@ class Properties(db.Model):
         }
 
 class Testimonials(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     author = db.Column(db.String(80), unique=False, nullable=False)
     rating = db.Column(db.Float, unique=False, nullable=False)
     review = db.Column(db.String(80), unique=False, nullable=False)
-    #image = db.Column(db.String(200), unique=False, nullable=False)
 
     def to_json(self):
         return {
